bb8eec08aed314abeaa0594f6c12043aad575b66
Saptarshi Chakraborty <chakra.saptarshi@gmail.com>
2021-05-02 12:04:05 -0400

    iniitial commit

---
+++ R/pvLRT-package.R
@@ -0,0 +1,16 @@
+#' The 'pvLRT' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name pvLRT-package
+#' @aliases pvLRT
+#' @useDynLib pvLRT, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
